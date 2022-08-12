a45282947058f516e47474e426f9964cae5519e8
Luis Daniel Mendez Castellanos <37163079+LuisDaniel2166@users.noreply.github.com>
2022-08-12 11:15:26 -0600

    The mapping of the different nativo functions and their corresponding storage and modification of information in SQL was carried out

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from genericpath import exists
 import json
 import os
 import re
@@ -22,16 +23,54 @@ def config(filename='database.ini', section='postgresql'):
     return db
 
 def init_db():
-    commands = """
-        CREATE TABLE IF NOT EXISTS nativo_indexer_data (
-            receipt_id  text        NOT NULL,
-            data        jsonb       NOT NULL,
-            contract    text        NOT NULL,
-            method      text        NOT NULL,
-            date        text   NOT NULL,
-            PRIMARY KEY (receipt_id, date)
-        )
+    commands =( 
         """
+        CREATE TABLE IF NOT EXISTS Collection (
+            collectionID    bigint          NOT NULL,
+            owner_id        text            NOT NULL,
+            title           text            NOT NULL,
+            description     text            NOT NULL,
+            tokenCount      bigint          NOT NULL,
+            salesCount      bigint          NOT NULL,
+            saleVolume      money           NOT NULL,
+            mediaIcon       text            NOT NULL,
+            mediaBanner     text            NOT NULL,
+            date            text            NOT NULL,
+            visibility      boolean         NOT NULL,
+            PRIMARY KEY (collectionID)
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS profile (
+            username        text        NOT NULL,
+            media           text        NOT NULL,
+            biography       text        NOT NULL,
+            tokCreated      bigint      NOT NULL,
+            tokBought       bigint      NOT NULL,
+            socialMedia     text        NOT NULL,
+            date            text        NOT NULL,
+            PRIMARY KEY (username)
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS token (
+            tokenId         bigint      NOT NULL,
+            contract        text        NOT NULL,
+            owner_id        text        NOT NULL,
+            title           text        NOT NULL,
+            description     text        NOT NULL,
+            media           text        NOT NULL,
+            creator         text        NOT NULL,
+            price           text        NOT NULL,
+            onSale          boolean     NOT NULL,
+            extra           text        NOT NULL,
+            approvalID      bigint      NOT NULL,
+            collectionID    bigint      NOT NULL,
+            date            text        NOT NULL,
+            PRIMARY KEY (tokenId)
+        )
+        """,
+        )
     
     conn = None
     try:
@@ -39,10 +78,8 @@ def init_db():
         print('Conectando a la base de datos')
         conn = psycopg2.connect(**params)
         cur=conn.cursor()
-        print(commands)
-        print(conn)
-        print(cur)
-        cur.execute(commands)
+        for command in commands:
+            cur.execute(command)
         cur.close()
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -50,20 +87,17 @@ def init_db():
     finally:
         if conn is not None:
             conn.close()
-            print('Database connection closed.')
+            print('Tablas creadas.')
 
-def insert_data(receipt,data,contract,method,date):
-    query = """
-            INSERT INTO nativo_indexer_data(receipt_id,data,contract,method,date)
-            VALUES(%s,%s,%s,%s,%s)
-            """
+
+def execute_insert(query):
     conn = None
-    
     try:
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute(query, (receipt,json.dumps(data),contract,method,date))
+        print(query)
+        cur.execute(query)
         conn.commit()
         cur.close()
         print('Log Insertado')
@@ -73,37 +107,6 @@ def insert_data(receipt,data,contract,method,date):
         if conn is not None:
             conn.close()
 
-def format_paras_nfts(data, receipt_execution_outcome):
-    links = []
-
-    for data_element in data:
-        for token_id in data_element.get("token_ids", []):
-            first_part_of_token_id = token_id.split(":")[0]
-            links.append(
-                f"https://paras.id/token/{receipt_execution_outcome.receipt.receiver_id}::{first_part_of_token_id}/{token_id}"
-            )
-
-    return {"owner": data[0].get("owner_id"), "links": links}
-
-
-def format_mintbase_nfts(data, receipt_execution_outcome):
-    links = []
-    for data_block in data:
-        try:
-            memo = json.loads(data_block.get("memo"))
-        except json.JSONDecodeError:
-            print(
-                f"Receipt ID: `{receipt_execution_outcome.receipt.receipt_id}`\nMemo: `{memo}`\nError during parsing Mintbase memo from JSON string to dict"
-            )
-            return
-
-        meta_id = memo.get("meta_id")
-        links.append(
-            f"https://www.mintbase.io/thing/{meta_id}:{receipt_execution_outcome.receipt.receiver_id}"
-        )
-
-    return {"owner": data[0].get("owner_id"), "links": links}
-
 
 async def handle_streamer_message(streamer_message: near_primitives.StreamerMessage):
     for shard in streamer_message.shards:
@@ -111,71 +114,53 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
             if receipt_execution_outcome.receipt.receiver_id == "v4.nativo-market.testnet":
                 for log in receipt_execution_outcome.execution_outcome.outcome.logs:
                     method=""
-                    if "collection_id" in log : 
+                    if "visibility" in log : 
                         if "create" in log :
                             method="add_new_user_collection-create"
+                            data = json.loads(log)['params']
+                            if 'visibility' not in data:
+                                data['visibility'] = 'true'
+                            query = "INSERT INTO collection(collectionid,date,description,mediabanner,mediaicon,owner_id,salescount,salevolume,title,tokencount,visibility) VALUES ("+str(data['collection_id'])+",'"+str(streamer_message.block.header.timestamp_nanosec)+"','"+str(data['description'])+"','"+str(data['media_banner'])+"','"+str(data['media_icon'])+"','"+str(data['owner_id'])+"',"+str(0)+","+str(0)+",'"+str(data['title'])+"',"+str(0)+","+str(data['visibility'])+")"
+                            execute_insert(query)
                         else:
                             method="add_new_user_collection-edit"
+                            data = json.loads(log)['params']
+                            query = "UPDATE collection SET description='"+str(data['description'])+"',mediabanner='"+str(data['media_banner'])+"',mediaicon='"+str(data['media_icon'])+"',title='"+str(data['title'])+"',visibility="+str(data['visibility'])+" WHERE collectionid="+str(data['collection_id'])
+                            execute_insert(query)
                     if "approval_id" in log :
                         method="add_token_to_collection"
+                        data = json.loads(log)['params']
+                        queryInsert= "INSERT INTO token(approvalid,collectionid,contract,creator,date,description,extra,media,onsale,owner_id,price,title,tokenid) VALUES ("+str(data['approval_id'])+","+str(data['collection_id'])+",'"+str(data['contract_id'])+"','"+str(data['creator'])+"','"+str(streamer_message.block.header.timestamp_nanosec)+"','"+str(data['description'])+"','','"+str(data['media'])+"',false,'"+str(data['owner_id'])+"','0','"+str(data['title'])+"',"+str(data['token_id'])+")"
+                        queryUpTokCol = "UPDATE collection SET tokencount=tokencount+1 WHERE collectionID="+str(data['collection_id'])
+                        queryUpTokCrea = "UPDATE profile SET tokcreated=tokcreated+1 WHERE username='"+str(data['creator'])+"'"
+                        execute_insert(queryInsert)
+                        execute_insert(queryUpTokCol)
+                        execute_insert(queryUpTokCrea)
                     if "username" in log :
                         if "create" in log :
                             method="add_new_profile-create"
+                            data = json.loads(log)['params']
+                            query = "INSERT INTO profile(biography,date,media,socialmedia,tokbought,tokcreated,username) VALUES ('"+str(data['biography'])+"','"+str(streamer_message.block.header.timestamp_nanosec)+"','"+str(data['media'])+"','"+str(data['social_media'])+"',0,0,'"+str(data['username'])+"')"
+                            execute_insert(query)
                         else:
                             method="add_new_profile-edit"
-                    output = {
-                        "receipt_id": receipt_execution_outcome.receipt.receipt_id,
-                        "data": json.loads(log),
-                        "contract": receipt_execution_outcome.receipt.receiver_id,
-                        "method": method
-                    }
-                    receipt = receipt_execution_outcome.receipt.receipt_id
-                    data = json.loads(log)
-                    contract = receipt_execution_outcome.receipt.receiver_id
-                    date = streamer_message.block.header.timestamp_nanosec
-                    insert_data(receipt,data,contract,method,date)
-                    print(json.dumps(output, indent=4))
+                            data = json.loads(log)['params']
+                            query = "UPDATE profile SET media='"+str(data['media'])+"',biography='"+str(data['biography'])+"',socialMedia='"+str(data['social_media'])+"' WHERE username='"+str(data['username'])+"'"
+                            execute_insert(query)
+                    # output = {
+                    #     "receipt_id": receipt_execution_outcome.receipt.receipt_id,
+                    #     "data": json.loads(log),
+                    #     "contract": receipt_execution_outcome.receipt.receiver_id,
+                    #     "method": method
+                    # }
+                    # receipt = receipt_execution_outcome.receipt.receipt_id
+                    # data = json.loads(log)
+                    # contract = receipt_execution_outcome.receipt.receiver_id
+                    # date = streamer_message.block.header.timestamp_nanosec
+                    # insert_data(receipt,data,contract,method,date)
+                    # print(json.dumps(output, indent=4))
             else:
                 continue
-
-                # if not log.startswith("EVENT_JSON:"):
-                #     continue
-                # try:
-                #     parsed_log = json.loads(log[len("EVENT_JSON:") :])
-                # except json.JSONDecodeError:
-                #     print(
-                #         f"Receipt ID: `{receipt_execution_outcome.receipt.receipt_id}`\nError during parsing logs from JSON string to dict"
-                #     )
-                #     continue
-
-                # if (
-                #     parsed_log.get("standard") != "nep171"
-                #     or parsed_log.get("event") != "nft_mint"
-                # ):
-                #     continue
-
-                # if receipt_execution_outcome.receipt.receiver_id.endswith(
-                #     ".paras.near"
-                # ):
-                #     output = {
-                #         "receipt_id": receipt_execution_outcome.receipt.receipt_id,
-                #         "marketplace": "Paras",
-                #         "nfts": format_paras_nfts(
-                #             parsed_log["data"], receipt_execution_outcome
-                #         ),
-                #     }
-                # elif re.search(
-                #     ".mintbase\d+.near", receipt_execution_outcome.receipt.receiver_id
-                # ):
-                #     output = {
-                #         "receipt_id": receipt_execution_outcome.receipt.receipt_id,
-                #         "marketplace": "Mintbase",
-                #         "nfts": format_mintbase_nfts(
-                #             parsed_log["data"], receipt_execution_outcome
-                #         ),
-                #     }
-                # else:
-                #     continue
 
                 
 
@@ -183,7 +168,7 @@ async def handle_streamer_message(streamer_message: near_primitives.StreamerMess
 async def main():
     init_db()
     config = LakeConfig.testnet()
-    config.start_block_height = 96734620
+    config.start_block_height = 97336124
     config.aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     config.aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
