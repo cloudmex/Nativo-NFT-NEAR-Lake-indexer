0a2df9185b78ce17dd5436a04c5b65769497a3b0
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2022-08-23 15:19:01 -0600

    the basic structure and the links were added to be able to continue with the work in the future

--- handlerLambda.js
+++ handlerLambda.js
@@ -18,35 +18,32 @@ async function handler(event) {
         ssl: true,
     };
 
-    let sql = postgres(connectionConfig);
+    // let sql = postgres(connectionConfig);
+    //Falta modifcar la conexion a la base de datos de postgreSQL
+    //Tambien se tiene que realizar la adaptacion de las querys y recuperacion de informacion
+    //Una manera de implementarlo esta en el siguiente enlace: https://ed.team/blog/como-usar-bases-de-datos-postgres-con-nodejs
     let payload = {};
 
-    let collectionColumns = event.info.selectionSetList.filter((item) => !item.startsWith("token"));
-    let [collection] = await sql`SELECT ${sql(collectionColumns)} FROM collection WHERE collectionid = ${event.arguments.id}`;
+    switch(event.info.fieldName){
+        case "getCollectionGallery":
+            let collectionColumns = event.info.selectionSetList.filter((item) => !item.startsWith("token"));
+            let [collection] = await sql`SELECT ${sql(collectionColumns)} FROM collection WHERE collectionid = ${event.arguments.id}`;
 
-    payload = { ...collection };
+            payload = { ...collection };
 
-    if (event.info.selectionSetList.some((item) => item === "token")) {
-        let tokenColumns = event.info.selectionSetList.filter((item) => item.startsWith("token/")).map((item) => item.split("/")[1]);
-        let [token] = await sql`SELECT ${sql(tokenColumns)} FROM token WHERE collectionid = ${collection.id}`;
+            if (event.info.selectionSetList.some((item) => item === "token")) {
+                let tokenColumns = event.info.selectionSetList.filter((item) => item.startsWith("token/")).map((item) => item.split("/")[1]);
+                let [token] = await sql`SELECT ${sql(tokenColumns)} FROM token WHERE collectionid = ${collection.id}`;
 
-        payload.token = { ...token };
+                payload.token = { ...token };
+            }
+            break
+        case "Query que ha sido llamada":
+            //Recoleccion de datos de la query a ejecutar
+            //Un ejemplo de como se desarrolla y como se manda se encuentra en la parte superior
+            //Para mas dudas checar este enlace: https://aws.amazon.com/es/blogs/mobile/appsync-graphql-sql-rds-proxy/
+            break
     }
-
-    // if (event.info.selectionSetList.some((item) => item === "car/parking")) {
-    //     let carParkingColumns = event.info.selectionSetList.filter((item) => item.startsWith("car/parking/")).map((item) => item.split("/")[2]);
-
-    //     if (carParkingColumns.every((col) => parking[col])) {
-    //         payload.car.parking = {};
-    //         carParkingColumns.forEach((col) => {
-    //             payload.car.parking[col] = parking[col];
-    //         });
-    //     } else {
-    //         let parkingExtraColumns = carParkingColumns.filter((item) => !parkingColumns.includes(item));
-    //         let [parkingExtraFields] = await sql`SELECT ${sql(parkingExtraColumns)} FROM Parking WHERE id = ${event.arguments.id}`;
-    //         payload.car.parking = { ...parking, ...parkingExtraFields };
-    //     }
-    // }
 
     await sql.end({ timeout: 0 });
 
